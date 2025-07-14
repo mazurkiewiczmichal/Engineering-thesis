8a5ca145f024bca75df75b412a908d7ec83174cd
Michal <mzrkwcz2001@gmail.com>
2025-07-14 12:39:08 +0200

    frontend water level status added

--- gardON.js
+++ gardON.js
@@ -1,6 +1,9 @@
 const modeSwitch = document.getElementById("modeSwitch");
 const scheduleMode = document.getElementById("scheduleMode");
 const manualMode = document.getElementById("manualMode");
+const tankLevel1 = document.getElementById("tankLevel1");
+const tankLevel2 = document.getElementById("tankLevel2");
+const tankLevel3 = document.getElementById("tankLevel3");
 
 modeSwitch.onchange = (event) => {
     const isScheduleMode = event.target.checked;
@@ -12,4 +15,6 @@ modeSwitch.onchange = (event) => {
         manualMode.classList.remove("hidden");
     }
 };
+
+tankLevel1.classList.add("filled");
 
