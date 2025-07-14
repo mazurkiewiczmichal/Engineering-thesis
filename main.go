c565bec1a72d0174242257c166b238858effdb41
Michal <mzrkwcz2001@gmail.com>
2025-07-14 14:34:17 +0200

    added form for schedule mode

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func main() {
 	mux := http.NewServeMux()
@@ -10,7 +14,26 @@ func main() {
 		http.ServeFile(w, r, "gardON.html")
 	})
 
-	// Serwowanie wszystkich plików z bieżącego katalogu (CSS, JS, PNG itd.)
+	mux.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "Błąd formularza", http.StatusBadRequest)
+			return
+		}
+
+		// Pobieranie wartości z formularza
+		days := r.Form["days"]
+		initialTime := r.FormValue("initialTime")
+		endTime := r.FormValue("endTime")
+
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		fmt.Fprintf(w, "Zaznaczone dni: %s<br>Od: %s<br>Do: %s",
+			strings.Join(days, ", "),
+			initialTime,
+			endTime,
+		)
+
+	})
+
 	fs := http.FileServer(http.Dir("."))
 	mux.Handle("/gardON.css", fs)
 	mux.Handle("/gardON.js", fs)
@@ -20,4 +43,5 @@ func main() {
 	mux.Handle("/logoONswitch.png", fs)
 
 	http.ListenAndServe(":12346", mux)
+
 }
