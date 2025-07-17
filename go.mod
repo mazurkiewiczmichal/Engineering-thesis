4e64a89b2bd5e3b18428fed1fcffff1274b6073a
Michal <mzrkwcz2001@gmail.com>
2025-07-17 17:03:57 +0200

    manual mode added switches and waterLevel to raspberry

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module engineering-thesis
 
 go 1.24.5
+
+require github.com/stianeikeland/go-rpio/v4 v4.6.0
--- main.go
+++ main.go
@@ -5,32 +5,61 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+var (
+	daysWeekday  []time.Weekday = []time.Weekday{}
+	days         []string       = []string{}
+	soilMoisture                = 22
+	waterLevel1                 = false
+	pinLevel1                   = rpio.Pin(4)
+	waterLevel2                 = false
+	pinLevel2                   = rpio.Pin(17)
+	waterLevel3                 = true
+	pinLevel3                   = rpio.Pin(27)
+	valveSwitch                 = true
+	valvePin                    = rpio.Pin(22)
+	pumpSwitch                  = true
+	pumpPin                     = rpio.Pin(10)
+	pouring                     = false
+	initialTime  string
+	endTime      string
 )
 
 func main() {
 
-	mux := http.NewServeMux()
+	pumpPin.Output()
+	valvePin.Output()
 
-	soilMoisture := 22
-	waterLevel1 := false
-	waterLevel2 := false
-	waterLevel3 := true
-	valveSwitch := true
-	pumpSwitch := true
-	pouring := false
+	pinLevel1.Input()
+	pinLevel2.Input()
+	pinLevel3.Input()
+
+	pinLevel1.PullUp()
+	pinLevel2.PullUp()
+	pinLevel3.PullUp()
+
+	mux := http.NewServeMux()
 
 	mux.HandleFunc("/pumpOn", func(w http.ResponseWriter, r *http.Request) {
 		pumpSwitch = true
+		pumpPin.High()
 	})
 	mux.HandleFunc("/pumpOff", func(w http.ResponseWriter, r *http.Request) {
 		pumpSwitch = false
+		pumpPin.Low()
 	})
 
 	mux.HandleFunc("/valveOn", func(w http.ResponseWriter, r *http.Request) {
 		valveSwitch = true
+		valvePin.High()
 	})
 	mux.HandleFunc("/valveOff", func(w http.ResponseWriter, r *http.Request) {
 		valveSwitch = false
+		valvePin.Low()
 	})
 
 	_ = valveSwitch
@@ -94,9 +123,12 @@ func main() {
 			return
 		}
 
-		days := r.Form["days"]
+		days = r.Form["days"]
+		daysToWeekday()
 		initialTime := r.FormValue("initialTime")
 		endTime := r.FormValue("endTime")
+
+		fmt.Println(isDayToday())
 
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		fmt.Fprintf(w, "Selected days: %s\nSince: %s\nUntil: %s",
@@ -117,3 +149,96 @@ func main() {
 	http.ListenAndServe(":12346", mux)
 
 }
+
+func isDayToday() bool {
+	for _, d := range daysWeekday {
+		if d == time.Now().Weekday() {
+			return true
+		}
+	}
+	return false
+}
+
+func daysToWeekday() {
+
+	for _, d := range days {
+		switch strings.ToLower(d) {
+		case "sunday":
+			daysWeekday = append(daysWeekday, time.Sunday)
+		case "monday":
+			daysWeekday = append(daysWeekday, time.Monday)
+		case "tuesday":
+			daysWeekday = append(daysWeekday, time.Tuesday)
+		case "wednesday":
+			daysWeekday = append(daysWeekday, time.Wednesday)
+		case "thursday":
+			daysWeekday = append(daysWeekday, time.Thursday)
+		case "friday":
+			daysWeekday = append(daysWeekday, time.Friday)
+		case "saturday":
+			daysWeekday = append(daysWeekday, time.Saturday)
+		default:
+			fmt.Println("Nieznany dzień:", d)
+		}
+	}
+}
+
+// -------------------------------wip------------------------------------------
+// func stringTimeToTime(timeToChange string) time.Time {
+// 	t, _ := time.Parse("15:04", timeToChange)
+// 	return t
+// }
+
+// func isNowTime() bool {
+// 	var now = time.Date(0, time.January, 1, time.Now().Hour(), time.Now().Minute(), 0, 0, time.UTC)
+// 	if stringTimeToTime(initialTime).After(now) && stringTimeToTime(endTime).Before(now) {
+// 		return true
+// 	}
+// 	return false
+// }
+
+// func counterToStart(initial time.Time) time.Timer {
+// 	var timerToStart *time.Timer
+// 	var now = time.Date(0, time.January, 1, time.Now().Hour(), time.Now().Minute(), 0, 0, time.UTC)
+// 	if isNowTime() {
+// 		timerToStart = time.NewTimer(0 * time.Second)
+// 	} else {
+// 		timerToStart = time.NewTimer(initial.Sub(now))
+// 	}
+// 	return *timerToStart
+// }
+
+// func counterToEnd(end time.Time) time.Timer {
+// 	var timerToEnd *time.Timer
+// 	var now = time.Date(0, time.January, 1, time.Now().Hour(), time.Now().Minute(), 0, 0, time.UTC)
+// 	timerToEnd = time.NewTimer(end.Sub(now))
+// 	return *timerToEnd
+// }
+
+// func scheduleModeOn() {
+
+// }
+// --------------------------------------------------------------------------------
+// func timeToStart(){
+// 	for _, d := range days{
+
+// 		if <
+// 	}
+// }
+
+// func isDayMached() {
+// 	ticker := time.NewTicker(10*time.Second)
+// 	for _, d := range days{
+// 		if strings.EqualFold(d, now.Weekday().String)
+// 		<- ticker.C
+// 	}
+
+// 	ttt := time.NewTimer()
+// }
+
+// func isTimeBetween(){
+// 	ticker := time.NewTicker(10*time.Second)
+// 	for {
+// 		if time.Now().Format("15:04")>=initialTime&&time.Now().Format("15:04") <=endTime
+// 	}
+// }
